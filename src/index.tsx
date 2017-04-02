3e63f2c66af69f215df28f5ea8b5951fc0b637ef
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2017-04-02 19:37:09 -0300

    Fix debugging configs

--- src/index.tsx
+++ src/index.tsx
@@ -2,43 +2,54 @@
 import * as React from 'react'
 import { Component } from 'react'
 import {
-    StyleSheet,
-    Text,
-    View,
+  StyleSheet,
+  Text,
+  View,
+  Button,
 } from 'react-native'
+import Comp from './Comp'
 
 interface Props {
-
+  
 }
 
 interface State {
-
+  
 }
 
 export default class App extends Component<Props, State> {
-    public render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.text}>
-                    Hello worlds
-                </Text>
-            </View>
-        )
-    }
+  myFunction(person: string, age: number) {
+    console.log(person)
+  }
+  
+  public render() {
+    return (
+      <View style={styles.container}>
+        <Comp />
+        <Button
+          title='Press me'
+          color='#FF0000'
+          onPress={() => this.myFunction('Teste', 123)}
+        />
+        <Text style={styles.text}>
+          
+        </Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'mediumaquamarine',
-    } as React.ViewStyle,
-
-    text: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-        color: 'darkorchid',
-    } as React.TextStyle,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'mediumaquamarine',
+  } as React.ViewStyle,
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: 'darkorchid',
+  } as React.TextStyle,
 })
